7da5de22781406834603f809e0a6a31779c50565
dhanashri <dpagar@lenovo.com>
2020-03-09 15:01:46 +0530

    Supporting electron version 7 Added the removed typeDefinition

--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
 
 import { ElectronShell } from "./models/ElectronShell";
+import { BrowserWindow, IpcMain, IpcRenderer } from "electron";
 
 interface Boundries {
     width: number;
     height: number;
 }
 
-export const registerMainProcessListener = (browserWindow : any, mainProcess :any) => {
+export const registerMainProcessListener = (browserWindow: BrowserWindow, mainProcess: IpcMain) => {
     const electronShell = new ElectronShell(browserWindow);
-    mainProcess.on('resize_app', (eventData: Boundries) => {
+    console.log("registering main process listener - npm module");
+
+    mainProcess.on('resize_app', (_event: any, eventData: Boundries) => {
+        console.log("EventData : ", eventData);
         electronShell.resize(eventData);
     });
 }
 
-export const registerWindowEventListener = (window: Window, ipcRenderer: any) => {
+export const registerWindowEventListener = (window: Window, ipcRenderer: IpcRenderer) => {
+    console.log("registering window listener");
+
     window.addEventListener('RESIZE_APP',
         (event: MessageEvent) => {
+            console.log("IPC Renderer sending: ", event.data);
             ipcRenderer.send("resize_app", event.data);
-        }, false);
+        });
 }
